451d9d7373beb2b02f4eec5b9fa78974020a9ffb
Konstantin Kuzin <wearebeard@gmail.com>
2022-06-25 15:38:10 +0300

    Connect db

--- bin/app.js
+++ bin/app.js
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 /* eslint-disable no-console */
 
+import 'dotenv/config';
+import mongoose from 'mongoose';
 import app from '../server/app.js';
 
 const port = process.env.PORT ?? 5000;
 
 app.listen(port, () => {
   console.log(`Server up on http://localhost:${port}/`);
+  mongoose.connect(process.env.DATABASE_URL);
+  console.log('Database connected!');
 });

d4b0530bf74d387c5120c1f2c4b7c91cc06b6805
Konstantin Kuzin <wearebeard@gmail.com>
2022-06-25 23:09:12 +0300

    Add post method

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,6 +1,14 @@
-export default (_req, res) => {
+import * as dao from '../dao/users.js';
+
+const mapping = {
+  POST: dao.create,
+};
+
+export default async (req, res) => {
   try {
-    res.json('It\'s working!');
+    const result = await mapping[req.method](req);
+
+    res.json(result);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }

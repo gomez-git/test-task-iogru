109153aeccd32433c0511c297f9ab40ba63ee9bd
Konstantin Kuzin <wearebeard@gmail.com>
2022-06-25 23:10:54 +0300

    Add getAll function

--- server/dao/users.js
+++ server/dao/users.js
@@ -1,5 +1,11 @@
 import User from '../models/User.js';
 
+export const getAll = async () => {
+  const users = await User.find();
+
+  return users;
+};
+
 export const create = async (req) => {
   const { login, password } = req.body;
   const user = await User.create({ login, password });

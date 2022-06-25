d3b79e41bafa169fa65b191045cd781fdebdc42f
Konstantin Kuzin <wearebeard@gmail.com>
2022-06-25 16:26:15 +0300

    Connect notFound controller

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import usersRouter from './users.js';
+import notFoundController from '../controllers/notFound.js';
 
 const router = new Router();
 
 router.use('/users', usersRouter);
+router.use('*', notFoundController);
 
 export default router;

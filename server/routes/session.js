651b0e0c9e88876422f5c2d25aa63fce1f87e371
Konstantin Kuzin <wearebeard@gmail.com>
2022-06-26 21:12:59 +0300

    Add delete method to root route

--- server/routes/session.js
+++ server/routes/session.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import authMiddleware from '../middlewares/auth.js';
+import checkMiddleware from '../middlewares/check.js';
 import * as sessionController from '../controllers/session.js';
 
 const router = new Router();
 
 router.route('/')
-  .post(authMiddleware, sessionController.login);
+  .post(authMiddleware, sessionController.login)
+  .delete(checkMiddleware, sessionController.logout);
 
 export default router;

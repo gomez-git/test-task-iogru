608c4d607a9a2d1a0a34f8aa925c34a11675ab99
Konstantin Kuzin <wearebeard@gmail.com>
2022-06-26 21:09:48 +0300

    Add session router

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import sessionRouter from './session.js';
 import usersRouter from './users.js';
 import notFoundController from '../controllers/notFound.js';
 
 const router = new Router();
 
+router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 router.use('*', notFoundController);
 

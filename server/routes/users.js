8a0c64bc8d4291f834e319d306302b71a2beb851
Konstantin Kuzin <wearebeard@gmail.com>
2022-06-26 21:11:21 +0300

    Add check middleware

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
+import checkMiddleware from '../middlewares/check.js';
 import usersController from '../controllers/users.js';
 
 const router = new Router();
 
 router.route('/:id')
-  .patch(usersController)
-  .delete(usersController);
+  .patch(checkMiddleware, usersController)
+  .delete(checkMiddleware, usersController);
 router.route('/')
   .get(usersController)
   .post(usersController);

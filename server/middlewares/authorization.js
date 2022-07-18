b028d8a728ab800271c8109da952fb62de715203
Konstantin Kuzin <wearebeard@gmail.com>
2022-07-19 01:08:08 +0300

    Upgrade handling errors

--- server/middlewares/authorization.js
+++ server/middlewares/authorization.js
@@ -2,14 +2,31 @@ import jwt from 'jsonwebtoken';
 
 export default (req, res, next) => {
   try {
-    const [, token] = req.headers.authorization.split(' ');
-    if (!token) {
+    if (!req.headers.authorization) {
       res.sendStatus(401);
+      return;
     }
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-
+    const [method, token] = req.headers.authorization.split(' ');
+    if (method !== 'Bearer') {
+      res.status(400).json({ message: 'Unsupported auth header' });
+      return;
+    }
+    const user = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    res.locals.user = user;
     next();
-  } catch (err) {
-    res.status(500).json({ message: err.message });
+  } catch ({ name, message }) {
+    if (name === 'TokenExpiredError') {
+      res.sendStatus(401);
+      return;
+    }
+    if (name === 'JsonWebTokenError') {
+      res.sendStatus(400);
+      return;
+    }
+    if (name === 'NotBeforeError') {
+      res.sendStatus(403);
+      return;
+    }
+    res.status(500).json({ message });
   }
 };

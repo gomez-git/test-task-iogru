e604705e92102d406605a8bba8b2ecd57cefdae2
Konstantin Kuzin <wearebeard@gmail.com>
2022-07-19 12:50:07 +0300

    Rename session to root

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,12 @@
 import { Router } from 'express';
-import sessionRouter from './session.js';
+import rootRouter from './root.js';
 import usersRouter from './users.js';
 import notFoundController from '../controllers/notFound.js';
 
 const router = new Router();
 
-router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
+router.use('/', rootRouter);
 router.use('*', notFoundController);
 
 export default router;
--- server/routes/root.js
+++ server/routes/root.js
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import authentication from '../middlewares/authentication.js';
 import authorization from '../middlewares/authorization.js';
-import * as sessionController from '../controllers/session.js';
+import * as rootController from '../controllers/root.js';
 
 const router = new Router();
 
 router.route('/')
-  .post(authentication, sessionController.login)
-  .delete(authorization, sessionController.logout);
+  .post(authentication, rootController.login)
+  .delete(authorization, rootController.logout);
 
 export default router;

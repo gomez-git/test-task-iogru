84a1f6fe567e72b39a4633fc79979651ac40f055
Konstantin Kuzin <wearebeard@gmail.com>
2022-07-19 13:15:42 +0300

    Add password secret

--- server/utils/encrypt.js
+++ server/utils/encrypt.js
@@ -1,7 +1,7 @@
 import { createHmac } from 'crypto';
 
 export default (password) => {
-  const hash = createHmac('sha512', 'iogru');
+  const hash = createHmac('sha256', process.env.PASSWORD_SECRET);
   hash.update(password);
   return hash.digest('hex');
 };

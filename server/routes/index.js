4ad08d3e57f11bd6f9d2aa4f014df564bd56a370
Konstantin Kuzin <wearebeard@gmail.com>
2022-07-20 15:59:45 +0300

    Add authorization to users route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import rootRouter from './root.js';
 import usersRouter from './users.js';
+import authorization from '../middlewares/authorization.js';
 import notFoundController from '../controllers/notFound.js';
 
 const router = new Router();
 
-router.use('/users', usersRouter);
+router.use('/users', authorization, usersRouter);
 router.use('/', rootRouter);
 router.use('*', notFoundController);
 
